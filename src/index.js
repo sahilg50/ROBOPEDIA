b9c64c10981bbadbca820884414d29e544e0aadc
sahilg50 <56653026+sahilg50@users.noreply.github.com>
2020-10-21 18:52:12 +0530

    cards made

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card.js';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots.js';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<div>
+			<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+			<Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+			<Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+			<Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
+			<Card id={robots[4].id} name={robots[4].name} email={robots[4].email} />
+		</div>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
