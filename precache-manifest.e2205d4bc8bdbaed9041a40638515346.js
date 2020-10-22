47abb94020f7c141b6342b2ce5b2860cf020bffe
sahilg50 <56653026+sahilg50@users.noreply.github.com>
2020-10-22 14:37:25 +0530

    Updates

---
+++ precache-manifest.e2205d4bc8bdbaed9041a40638515346.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df9ae20676e058c8c697e21702b89cf",
+    "url": "/ROBOPEDIA/index.html"
+  },
+  {
+    "revision": "4419e48c2303975c5d9b",
+    "url": "/ROBOPEDIA/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "98b9f2828d86fb2b1e11",
+    "url": "/ROBOPEDIA/static/css/main.f1d9e865.chunk.css"
+  },
+  {
+    "revision": "4419e48c2303975c5d9b",
+    "url": "/ROBOPEDIA/static/js/2.36ed7a01.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/ROBOPEDIA/static/js/2.36ed7a01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b9f2828d86fb2b1e11",
+    "url": "/ROBOPEDIA/static/js/main.e62feb1d.chunk.js"
+  },
+  {
+    "revision": "7e4ae597be90d92e89da",
+    "url": "/ROBOPEDIA/static/js/runtime-main.737444bc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ROBOPEDIA/static/media/SEGA.12a3db8b.woff"
+  }
+]);
